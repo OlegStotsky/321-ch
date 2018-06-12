cf2af5ecba75a35c0eaaebf96a4c4c64dd68de36
Oleg Stotsky <ostotsky@gmail.com>
2018-06-12 16:15:41 +0300

    add background particles

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,10 +1,25 @@
 import * as React from "react";
+import Particles from "react-particles-js";
 import AppRouter from "../router/AppRouter";
 
-const App = () => (
-  <div>
-    <AppRouter />
-  </div>
-);
+const App = () => {
+  const particlesParams = {
+    particles: {
+      number: {
+        density: {
+          value_area: 100
+        },
+        value: 150
+      }
+    }
+  };
+
+  return (
+    <div>
+      <Particles params={particlesParams} className="bg-particles" />
+      <AppRouter />
+    </div>
+  );
+};
 
 export default App;
