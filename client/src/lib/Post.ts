dc67a339be6c72b2dfd95595c24e3522c3ea1372
Oleg Stotsky <ostotsky@gmail.com>
2018-08-22 21:13:51 +0400

    add client Post model

---
+++ client/src/lib/Post.ts
@@ -0,0 +1,23 @@
+import {
+  Decoder,
+  object,
+  string,
+  optional,
+  number
+} from "@mojotech/json-type-validation";
+
+export interface IPost {
+  date: number;
+  postNumber: number;
+  authorName: string;
+  content: string;
+  subject?: string;
+}
+
+export const postDecoder: Decoder<IPost> = object({
+  date: number(),
+  postNumber: number(),
+  authorName: string(),
+  content: string(),
+  subject: optional(string())
+});
