edd79d8745f1ebe2fc80c9288a5ff38908d05adb
Oleg Stotsky <ostotsky@gmail.com>
2018-08-22 21:13:01 +0400

    add ApiAdapter

---
+++ client/src/lib/ApiAdapter.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { array } from "@mojotech/json-type-validation";
+import { IBoardCredentials } from "../../../shared/lib/types/BoardCredentials";
+import { IThread, threadDecoder } from "./Thread";
+
+export default class ApiAdapter {
+  public static getThread(
+    boardCredentials: IBoardCredentials,
+    threadNumber: number
+  ): Promise<IThread> {
+    console.log(`/api/${boardCredentials.shortName}/${threadNumber}`);
+    return axios
+      .get(`/api/${boardCredentials.shortName}/${threadNumber}`)
+      .then(response => response.data)
+      .then(thread => threadDecoder.runWithException(thread));
+  }
+}
