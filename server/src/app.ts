60e1d8fd9b27a40b531d6880ebc6ab2c1a3490e4
Oleg Stotsky <ostotsky@gmail.com>
2018-08-25 00:22:50 +0400

    add morgan-body logger middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,12 +3,15 @@ import mongoose from "mongoose";
 import config from "./config/config";
 import apiRouter from "./routes/api";
 import * as bodyParser from "body-parser";
+import morganBody from "morgan-body";
+import morgan from "morgan";
 import Thread from "./models/thread";
 import Board from "./models/Board";
 
 const app = express();
 
 app.use(bodyParser.json());
+morganBody(app);
 
 mongoose.connect(config["mongo-uri"]);
 mongoose.connection.once("connected", () => {
