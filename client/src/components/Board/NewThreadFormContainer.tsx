e1b931927a67b92b25625dfb6d1a8e2fd14d2bd8
Oleg Stotsky <ostotsky@gmail.com>
2018-08-25 00:20:49 +0400

    rename ThreadForm to NewThreadForm

---
+++ client/src/components/Board/NewThreadFormContainer.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+import NewThreadForm from "./NewThreadForm";
+
+interface IThreadFormContainerState {
+  authorName: string;
+  threadName: string;
+  message: string;
+}
+
+const lengthConstrains = {
+  authorName: 30,
+  threadName: 50,
+  message: 1500
+};
+
+class NewThreadFormContainer extends React.Component<
+  {},
+  IThreadFormContainerState
+> {
+  public state: IThreadFormContainerState = {
+    authorName: "Anonymous",
+    threadName: "",
+    message: ""
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  public onAuthorNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    if (value.length < lengthConstrains[name]) {
+      this.setState(() => ({
+        authorName: value
+      }));
+    }
+  };
+
+  public onThreadNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    if (value.length < lengthConstrains[name]) {
+      this.setState(() => ({
+        threadName: value
+      }));
+    }
+  };
+
+  public onMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.currentTarget;
+    if (value.length < lengthConstrains[name]) {
+      this.setState(() => ({
+        message: value
+      }));
+    }
+  };
+
+  public onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  public render() {
+    return (
+      <NewThreadForm
+        onAuthorNameChange={this.onAuthorNameChange}
+        onThreadNameChange={this.onThreadNameChange}
+        onMessageChange={this.onMessageChange}
+        onSubmit={this.onSubmit}
+        authorName={this.state.authorName}
+        threadName={this.state.threadName}
+        message={this.state.message}
+      />
+    );
+  }
+}
+
+export default NewThreadFormContainer;
