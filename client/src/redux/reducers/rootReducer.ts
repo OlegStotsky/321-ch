0169812c5ff4b32ca03e446c927c15031dbed366
Oleg Stotsky <ostotsky@gmail.com>
2018-08-26 03:18:39 +0400

    update IRootReducer and createStore to support flashMessages

--- client/src/redux/reducers/rootReducer.ts
+++ client/src/redux/reducers/rootReducer.ts
@@ -1,7 +1,11 @@
 import { ICurThreadState } from "./curThread";
 import { ICurBoardState } from "./curBoard";
+import { IApiState } from "./api";
+import { IFlashMessagesState } from "./flashMessages";
 
 export interface IRootState {
   curThread: ICurThreadState;
   curBoard: ICurBoardState;
+  api: IApiState;
+  flashMessages: IFlashMessagesState;
 }
--- client/src/redux/store/createStore.ts
+++ client/src/redux/store/createStore.ts
@@ -6,13 +6,17 @@ import {
 import thunk from "redux-thunk";
 import curBoardReducer from "../reducers/curBoard";
 import curThreadReducer from "../reducers/curThread";
+import { apiReducer } from "../reducers/api";
+import { flashMessagesReducer } from "../reducers/flashMessages";
 
 export default () => {
   const composeEnhancers = composeWithDevTools({});
   const store = createStore(
     combineReducers({
       curBoard: curBoardReducer,
-      curThread: curThreadReducer
+      curThread: curThreadReducer,
+      api: apiReducer,
+      flashMessages: flashMessagesReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
   );
