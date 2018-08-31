680155cd33c46e833856625a484c44fc707b7f4d
Oleg Stotsky <ostotsky@gmail.com>
2018-09-01 03:18:44 +0400

    refactor api router: move logic to ApiController

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response } from "express";
 import * as express from "express";
-import * as R from "ramda";
 import { IThreadDocument, IThread } from "../models/Thread";
 import Board from "../models/Board";
 import { IPostDocument } from "../models/Post";
@@ -11,87 +10,13 @@ import {
   ThreadNotFoundError
 } from "../lib/apiServiceExceptions";
 import { pickValuesFromPost, pickValuesfromThread } from "../utils/utils";
+import ApiController from "../controllers/apiController";
 
 const apiRouter: Router = Router();
+const apiController = new ApiController();
 
-apiRouter.post("/:board_name/", (req: Request, res: Response) => {
-  const { opPostAuthor, opPostSubject, opPostContent } = req.body;
-  Board.findOne({ name: req.params.board_name }).then(board => {
-    if (!board) {
-      res.status(400).json({
-        errors: "Board doesn't exist"
-      });
-    }
-
-    board
-      .addThread({ opPostAuthor, opPostContent, opPostSubject })
-      .then(thread => {
-        thread
-          .populate("opPost")
-          .execPopulate()
-          .then(populatedThread => {
-            res.status(201).json(pickValuesFromPost(populatedThread.opPost));
-          });
-      })
-      .catch(e => {
-        res.status(400).json({
-          errors: e.errors
-        });
-      });
-  });
-});
-
-apiRouter.get(
-  "/:boardName/:threadNumber",
-  async (req: Request, res: Response) => {
-    const boardName: string = req.params.boardName;
-    const threadNumber: number = parseInt(req.params.threadNumber, 10);
-    try {
-      const thread = await findThreadInBoard(boardName, threadNumber);
-      thread.populateThread().then(populatedThread => {
-        res.status(200).send(pickValuesfromThread(thread));
-      });
-    } catch (e) {
-      if (
-        !(e instanceof BoardNotFoundError || e instanceof ThreadNotFoundError)
-      ) {
-        throw e;
-      }
-      res.status(400).json({
-        error: e.message
-      });
-    }
-  }
-);
-
-apiRouter.post(
-  "/:boardName/:threadNumber/",
-  async (req: Request, res: Response) => {
-    const boardName: string = req.params.boardName;
-    const threadNumber: number = parseInt(req.params.threadNumber, 10);
-    try {
-      const thread: IThread = await findThreadInBoard(boardName, threadNumber);
-      thread
-        .addPost(req.body)
-        .then(newPost => {
-          res.status(201).json(pickValuesFromPost(newPost));
-        })
-        .catch(e => {
-          res.status(400).json({
-            errors: ["Something went wrong"]
-          });
-        });
-    } catch (e) {
-      if (
-        !(e instanceof BoardNotFoundError || e instanceof ThreadNotFoundError)
-      ) {
-        throw e;
-      }
-      res.status(400).json({
-        error: e.message
-      });
-    }
-  }
-);
+apiRouter.post("/:board_name/", apiController.createNewThread);
+apiRouter.get("/:boardName/:threadNumber", apiController.findThread);
+apiRouter.post("/:boardName/:threadNumber/", apiController.createNewPost);
 
 export default apiRouter;
