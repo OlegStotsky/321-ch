884586d6e90d6d84440c18dbdeadd22d7eae0999
Oleg Stotsky <ostotsky@gmail.com>
2018-09-02 02:58:24 +0400

    api router: add get "/:boardName" route

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -4,8 +4,9 @@ import ApiController from "../controllers/apiController";
 const apiRouter: Router = Router();
 const apiController = new ApiController();
 
-apiRouter.post("/:board_name/", apiController.createNewThread);
 apiRouter.get("/:boardName/:threadNumber", apiController.findThread);
+apiRouter.get("/:boardName/", apiController.findAllThreads);
+apiRouter.post("/:board_name/", apiController.createNewThread);
 apiRouter.post("/:boardName/:threadNumber/", apiController.createNewPost);
 
 export default apiRouter;
