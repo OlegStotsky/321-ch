1afba9619bbf2f885fb0ecb44094f48ae8d8a4c8
Oleg Stotsky <ostotsky@gmail.com>
2018-09-01 19:10:04 +0400

    refactor  api router: remove unneccessary imports

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,15 +1,4 @@
 import { Router, Request, Response } from "express";
-import * as express from "express";
-import { IThreadDocument, IThread } from "../models/Thread";
-import Board from "../models/Board";
-import { IPostDocument } from "../models/Post";
-import Thread from "../models/Thread";
-import { findThreadInBoard } from "../lib/apiService";
-import {
-  BoardNotFoundError,
-  ThreadNotFoundError
-} from "../lib/apiServiceExceptions";
-import { pickValuesFromPost, pickValuesfromThread } from "../utils/utils";
 import ApiController from "../controllers/apiController";
 
 const apiRouter: Router = Router();
