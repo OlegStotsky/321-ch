6682c1943a825899c3264c891b8100eb972f6eee
Oleg Stotsky <ostotsky@gmail.com>
2018-09-06 11:01:40 +0400

    hide server

--- server/src/config/config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  "mongo-uri": process.env.MONGODB_URI || "mongodb://localhost:27017/321ch",
-  "mongo-test-uri":
-    process.env.MONGODB_URI || "mongodb://localhost:27017/321ch-test"
-};
--- server/src/server.ts
+++
@@ -1,5 +0,0 @@
-import app from "./app";
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("listening on 3000");
-});
