9d9e51d482166ee4a21184248d6162f2ecba1f4d
Oleg Stotsky <ostotsky@gmail.com>
2018-09-06 11:11:07 +0400

    update gitignore: remove config

---
+++ server/src/config/config.ts
@@ -0,0 +1,5 @@
+export default {
+  "mongo-uri": process.env.MONGODB_URI || "mongodb://localhost:27017/321ch",
+  "mongo-test-uri":
+    process.env.MONGODB_URI || "mongodb://localhost:27017/321ch-test"
+};
