1c2cb90637e3ba6feae5c4938e658b7500cf7ccb
Oleg Stotsky <ostotsky@gmail.com>
2018-09-08 08:27:40 +0400

    server: add errorHandler
    This closes #26.

---
+++ server/src/lib/errorHandler.ts
@@ -0,0 +1,11 @@
+import { AppError } from "./appError";
+import { logger } from "../config/winston";
+
+export const errorHandler = (error: AppError) => {
+    if (error.isOperational) {
+        logger.warn(error.message);
+    } else {
+        logger.error(error.message);
+        throw error;
+    }
+};
