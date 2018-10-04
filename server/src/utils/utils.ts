341632e1d856d89b2585a40b455ef4e784782a20
Oleg Stotsky <ostotsky@gmail.com>
2018-10-04 23:49:05 +1000

    add saveBase64toFile function

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -1,6 +1,8 @@
 import { IPostDocument } from "../models/Post";
 import * as R from "ramda";
 import { IThreadDocument } from "../models/Thread";
+import { writeFile } from "fs-promise";
+import * as path from "path";
 
 export const pickValuesFromPost = (post: IPostDocument) => {
   return R.pick(
@@ -14,4 +16,14 @@ export const pickValuesfromThread = (thread: IThreadDocument) => {
     { opPost: pickValuesFromPost, posts: R.map(pickValuesFromPost) },
     R.pick(["posts", "opPost", "opPostNumber"], thread)
   );
+};
+
+export const saveBase64ToDisk = (
+  dir: string,
+  fileName: string,
+  data: string
+) => {
+  const img = data.replace(/^data:image\/\w+;base64,/, "");
+  const buf = new Buffer(img, "base64");
+  return writeFile(path.join(dir, fileName), buf);
 };
