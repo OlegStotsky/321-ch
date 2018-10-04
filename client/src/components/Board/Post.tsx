ba61f1f799466e75b1a156a00734c6cd27d07cad
Oleg Stotsky <ostotsky@gmail.com>
2018-10-04 17:53:47 +1000

    add Image component

--- client/src/components/Board/Post.tsx
+++ client/src/components/Board/Post.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as moment from "moment";
 import PostId from "../../../../shared/lib/types/PostId";
+import PostImage from "./PostImage";
 
 interface IPostProps {
   date: number;
@@ -33,7 +34,10 @@ const Post: React.SFC<IPostProps> = ({
           <span className="post__number">{`No. ${postNumber}`}</span>
           <span className="post__author">{authorName}</span>
         </div>
-        <div className="post__body">{content}</div>
+        <div className="post__body">
+          <PostImage url="https://i.pinimg.com/736x/d4/e8/d3/d4e8d3daab9456b8cdd5d08429c73aa9.jpg" />
+          <div className="post__text">{content}</div>
+        </div>
       </div>
     </div>
   );
